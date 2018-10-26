f18b4d2a6ead2deb9285fe24f231236a42e1cfa3
valucifer89 <vvivone@indracompany.com>
2018-10-26 17:37:33 +0200

    commit

---
+++ PHP/creazioneDirectory.php
@@ -0,0 +1,20 @@
+<?php
+    include (dirname(__DIR__)."/PHP/_configuration/configuration.php");
+    
+    $name_directory = __BASE_DIR__;
+    $root_directory = str_replace("/PHP","",$name_directory);
+    
+    if (!file_exists($root_directory)) {
+        mkdir($root_directory, 0777, true);
+    }
+    
+    echo "root: ".$root_directory."<br>";
+    
+    $project = "carmel";
+    
+    if (!file_exists($root_directory.'/'.$project)) {
+        mkdir($root_directory.'/'.$project, 0777, true);
+    }
+    echo "dirproj: ".$root_directory.'/'.$project."<br>";
+    
+?>
